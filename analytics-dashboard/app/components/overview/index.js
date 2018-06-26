16ba0783fc95a4de6a6280c27b4ce91bdcda4ae9
Katerina Roukounaki <k4t3r1n4@gmail.com>
2018-06-26 18:23:44 +0300

    Implement the rest of the counters

--- analytics-dashboard/app/components/overview/index.js
+++ analytics-dashboard/app/components/overview/index.js
@@ -4,6 +4,12 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { Card, Container, Icon, Label } from 'semantic-ui-react';
 
+import analyticsInstances from 'actions/analytics-instances';
+import analyticsProcessorDefinitions from 'actions/analytics-processor-definitions';
+import dataInterfaces from 'actions/data-interfaces';
+import dataKinds from 'actions/data-kinds';
+import dataSourceDefinitions from 'actions/data-source-definitions';
+import dataSources from 'actions/data-sources';
 import edgeGateways from 'actions/edge-gateways';
 import { tr } from 'helpers/languages';
 import messages from 'helpers/messages';
@@ -17,7 +23,115 @@ class Overview extends Component {
   }
 
   componentDidMount() {
+    this.fetchAnalyticsInstances();
+    this.fetchAnalyticsProcessorDefinitions();
+    this.fetchDataInterfaces();
+    this.fetchDataKinds();
+    this.fetchDataSourceDefinitions();
+    this.fetchDataSources();
     this.fetchEdgeGateways();
+  }
+
+  fetchAnalyticsInstances() {
+    // eslint-disable-next-line no-console
+    console.log('Fetch the analytics instances.');
+    send({
+      url: `${ process.env.OPEN_API_FOR_ANALYTICS_BASE_URL }/analytics-instances/discover`,
+      method: 'POST',
+      data: { }
+    }).then((response) => {
+      const analyticsInstances = response.data.analyticsInstances;
+      this.props.setAnalyticsInstances(analyticsInstances);
+    }).catch((error) => {
+      // eslint-disable-next-line no-console
+      console.error('Failed to fetch the analytics instances.', error);
+      messages.error(`${ tr('FAILED_TO_FETCH_ANALYTICS_INSTANCES') } ${ tr(error.message) }`);
+    });
+  }
+
+  fetchAnalyticsProcessorDefinitions() {
+    // eslint-disable-next-line no-console
+    console.log('Fetch the analytics processor definitions.');
+    send({
+      url: `${ process.env.MODEL_REPOSITORY_BASE_URL }/analytics-processor-definitions/discover`,
+      method: 'POST',
+      data: { }
+    }).then((response) => {
+      const analyticsProcessorDefinitions = response.data.analyticsProcessorDefinitions;
+      this.props.setAnalyticsProcessorDefinitions(analyticsProcessorDefinitions);
+    }).catch((error) => {
+      // eslint-disable-next-line no-console
+      console.error('Failed to fetch the analytics processor definitions.', error);
+      messages.error(`${ tr('FAILED_TO_FETCH_ANALYTICS_PROCESSOR_DEFINITIONS') } ${ tr(error.message) }`);
+    });
+  }
+
+  fetchDataInterfaces() {
+    // eslint-disable-next-line no-console
+    console.log('Fetch the data interfaces.');
+    send({
+      url: `${ process.env.MODEL_REPOSITORY_BASE_URL }/data-interfaces/discover`,
+      method: 'POST',
+      data: { }
+    }).then((response) => {
+      const dataInterfaces = response.data.dataInterfaces;
+      this.props.setDataInterfaces(dataInterfaces);
+    }).catch((error) => {
+      // eslint-disable-next-line no-console
+      console.error('Failed to fetch the data interfaces.', error);
+      messages.error(`${ tr('FAILED_TO_FETCH_DATA_INTERFACES') } ${ tr(error.message) }`);
+    });
+  }
+
+  fetchDataKinds() {
+    // eslint-disable-next-line no-console
+    console.log('Fetch the data kinds.');
+    send({
+      url: `${ process.env.MODEL_REPOSITORY_BASE_URL }/data-kinds/discover`,
+      method: 'POST',
+      data: { }
+    }).then((response) => {
+      const dataKinds = response.data.dataKinds;
+      this.props.setDataKinds(dataKinds);
+    }).catch((error) => {
+      // eslint-disable-next-line no-console
+      console.error('Failed to fetch the data kinds.', error);
+      messages.error(`${ tr('FAILED_TO_FETCH_DATA_KINDS') } ${ tr(error.message) }`);
+    });
+  }
+
+  fetchDataSourceDefinitions() {
+    // eslint-disable-next-line no-console
+    console.log('Fetch the data source definitions.');
+    send({
+      url: `${ process.env.MODEL_REPOSITORY_BASE_URL }/data-source-definitions/discover`,
+      method: 'POST',
+      data: { }
+    }).then((response) => {
+      const dataSourceDefinitions = response.data.dataSourceDefinitions;
+      this.props.setDataSourceDefinitions(dataSourceDefinitions);
+    }).catch((error) => {
+      // eslint-disable-next-line no-console
+      console.error('Failed to fetch the data source definitions.', error);
+      messages.error(`${ tr('FAILED_TO_FETCH_DATA_SOURCE_DEFINITIONS') } ${ tr(error.message) }`);
+    });
+  }
+
+  fetchDataSources() {
+    // eslint-disable-next-line no-console
+    console.log('Fetch the data sources.');
+    send({
+      url: `${ process.env.OPEN_API_FOR_ANALYTICS_BASE_URL }/data-sources/discover`,
+      method: 'POST',
+      data: { }
+    }).then((response) => {
+      const dataSources = response.data.dataSources;
+      this.props.setDataSources(dataSources);
+    }).catch((error) => {
+      // eslint-disable-next-line no-console
+      console.error('Failed to fetch the data sources.', error);
+      messages.error(`${ tr('FAILED_TO_FETCH_DATA_SOURCES') } ${ tr(error.message) }`);
+    });
   }
 
   fetchEdgeGateways() {
@@ -72,11 +186,11 @@ class Overview extends Component {
               </Label>
               <Label>
                 { tr('DATA_SOURCES') }
-                <Label.Detail>0</Label.Detail>
+                <Label.Detail>{ this.props.dataSources.length }</Label.Detail>
               </Label>
               <Label>
                 { tr('ANALYTICS_INSTANCES') }
-                <Label.Detail>0</Label.Detail>
+                <Label.Detail>{ this.props.analyticsInstances.length }</Label.Detail>
               </Label>
             </Card.Content>
           </Card>
@@ -101,11 +215,23 @@ class Overview extends Component {
                   <Card.Content extra className='statistics'>
                     <Label>
                       { tr('DATA_SOURCES') }
-                      <Label.Detail>42</Label.Detail>
+                      <Label.Detail>
+                        {
+                          this.props.dataSources.filter((ds) => {
+                            return ds.edgeGatewayReferenceID === edgeGateway.id;
+                          }).length
+                        }
+                      </Label.Detail>
                     </Label>
                     <Label>
                       { tr('ANALYTICS_INSTANCES') }
-                      <Label.Detail>42</Label.Detail>
+                      <Label.Detail>
+                        {
+                          this.props.analyticsInstances.filter((ai) => {
+                            return ai.edgeGatewayReferenceID === edgeGateway.id;
+                          }).length
+                        }
+                      </Label.Detail>
                     </Label>
                   </Card.Content>
                 </Card>
@@ -123,16 +249,30 @@ const mapStateToProps = (state) => {
   return {
     language: state.settings.language,
     analyticsProcessorDefinitions: state.analyticsProcessorDefinitions.analyticsProcessorDefinitions,
+    analyticsInstances: state.analyticsInstances.analyticsInstances,
     dataKinds: state.dataKinds.dataKinds,
     dataInterfaces: state.dataInterfaces.dataInterfaces,
     dataSourceDefinitions: state.dataSourceDefinitions.dataSourceDefinitions,
+    dataSources: state.dataSources.dataSources,
     edgeGateways: state.edgeGateways.edgeGateways
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
+  const _analyticsInstances = bindActionCreators(analyticsInstances, dispatch);
+  const _analyticsProcessorDefinitions = bindActionCreators(analyticsProcessorDefinitions, dispatch);
+  const _dataInterfaces = bindActionCreators(dataInterfaces, dispatch);
+  const _dataKinds = bindActionCreators(dataKinds, dispatch);
+  const _dataSourceDefinitions = bindActionCreators(dataSourceDefinitions, dispatch);
+  const _dataSources = bindActionCreators(dataSources, dispatch);
   const _edgeGateways = bindActionCreators(edgeGateways, dispatch);
   return {
+    setAnalyticsInstances: _analyticsInstances.setAnalyticsInstances,
+    setAnalyticsProcessorDefinitions: _analyticsProcessorDefinitions.setAnalyticsProcessorDefinitions,
+    setDataInterfaces: _dataInterfaces.setDataInterfaces,
+    setDataKinds: _dataKinds.setDataKinds,
+    setDataSourceDefinitions: _dataSourceDefinitions.setDataSourceDefinitions,
+    setDataSources: _dataSources.setDataSources,
     setEdgeGateways: _edgeGateways.setEdgeGateways
   };
 };
